1168a88758d6059c13b14b45e554ad460b6b2b8b
Sandeep B <sandeepb@reflections.local>
2020-10-21 13:53:34 +0530

    updates

--- static/script/script.js
+++ static/script/script.js
@@ -37,4 +37,26 @@ function main() {
     
 }
 
+function copytoclipboard(){
+    /* Get the text field */
+  var copyText = document.getElementById("extractedtext").innerText;
+  console.log(copyText)
 
+//   document.querySelector('#extractedtext').select();
+  /* Select the text field */
+//   copyText.select();
+//   copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+}
+
+function hide(x) {
+    // var hideMe = document.getElementById('hideMe');
+    // document.onclick = function(e){
+    //    if(e.target.id === 'hideMe'){
+    //       hideMe.style.display = 'none';
+    //    }
+    // };
+    x.style.display = 'none';
+}
